28788a5442b0b5ba0f2ffc24eaf5593af754b348
Mauricio Koller <39862225+mauriciokoller87@users.noreply.github.com>
2019-05-01 18:49:08 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>TpFinal</title>
+    <script src="https://code.jquery.com/jquery-3.4.0.js"
+  integrity="sha256-DYZMCC8HTC+QDr5QNaIcfR7VSPtcISykd+6eSmBW5qo="
+  crossorigin="anonymous"></script>
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/estilos.css">
+  </head>
+  <body>
+<div class="container">
+  <header>
+    <?php include("nav.php"); ?>
+  </header>
+      <main>
+        <h2>Bienvenidos a</h2>
+        <h1>Solo Notebooks</h1>
+      </main> <br>
+      <section id="notebooks">
+
+        <div class="productos">
+          <article class="producto">
+            <img src="img/notebook2.jpg" alt="">
+            <h3>Nuestros equipos</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente beatae dolorem, magnam quia odio! Error.</p>
+          </article>
+          <article class="producto">
+            <img src="img/acces.jpg" alt="">
+            <h3>Accesorios</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente beatae dolorem, magnam quia odio! Error.</p>
+          </article>
+          <article class="producto">
+            <img src="img/repuestos.png" alt="">
+            <h3>Repuestos</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente beatae dolorem, magnam quia odio! Error.</p>
+          </article>
+        </div>
+      </section>
+</div>
+  </body>
+</html>
