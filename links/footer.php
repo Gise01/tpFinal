3f2d3da627d2c2d27bba6b0eb7ff6c50f9bedfa8
Mauricio Koller <39862225+mauriciokoller87@users.noreply.github.com>
2019-06-04 01:51:02 -0300

    Add files via upload

--- links/footer.php
+++ links/footer.php
@@ -5,6 +5,9 @@
         <a class="nav-link" href="index.php">Solo Notebooks</a>
       </li>
       <li class="nav-item">
+        <a class="nav-link" href="comprar.php">Compra On Line</a>
+      </li>
+      <li class="nav-item">
         <a class="nav-link" href="login.php">Login</a>
       </li>
       <li class="nav-item">
--- links/nav.php
+++ links/nav.php
@@ -7,6 +7,9 @@ Solo Notebooks</a>
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
+        <a class="nav-link" href="comprar.php"> Compra On Line <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
         <a class="nav-link" href="login.php"> Login <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item active">
